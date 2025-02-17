ea645103bac1edb09cd4f4db5ba9722aea15caaa
Iberedem <ibee2k12@gmail.com>
2025-02-17 07:32:59 -0700

    Gallery and Footer Component Updates

--- src/components/ComponentName/Gallery/Gallery.jsx
+++ src/components/ComponentName/Gallery/Gallery.jsx
@@ -1,8 +1,23 @@
 import './Gallery.scss';
-import Photographs from '../Photographs/Photographs.jsx';
+// import Photographs from '../Photographs';
+// import photoData from "../../data/photos.json";
 
-const Gallery = () =>{
-    // return();
+const Gallery = ({selectedFilters, isFilterOpen}) =>{
+let filteredPhotos = photoData;
+
+if (selectedFilters.size > 0) {
+    filteredPhotos = photoData.filter((photo) =>
+        photo.tags.some((tag) => selectedFilters.has(tag))
+);
 }
+
+return(
+    <div className={`gallery ${isFilterOpen ? "gallery--filtered": ""}`}>
+        {filteredPhotos.map((photo, index) =>(
+            <Photograph key={index} photo={photo}/>
+        ))}
+    </div>
+     );
+    }
 
 export default Gallery;
